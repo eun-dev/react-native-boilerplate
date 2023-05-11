04cf9609255dbe94ce73e4fbc16775799c1071e3
seongeun <seongeun@seongeunui-MacBookPro.local>
2023-05-11 21:07:43 +0900

    .

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
 import * as SplashScreen from "expo-splash-screen";
 import RootNavigator from "./src/navigation/RootNavigator";
+import { ThemeProvider } from "styled-components/native";
+import { useColorScheme } from "react-native";
+import { darkTheme, lightTheme } from "./styled";
 
 SplashScreen.preventAutoHideAsync();
 
 const App = () => {
+  const isDark = useColorScheme() === "dark";
+
   const [ready, setReady] = useState<boolean>(false);
 
   const prepare = async () => {
@@ -24,7 +29,11 @@ const App = () => {
     }
   }, [ready]);
 
-  return <RootNavigator />;
+  return (
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <RootNavigator />;
+    </ThemeProvider>
+  );
 };
 
 export default App;
